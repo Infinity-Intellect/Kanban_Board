4ce08ae0cd17a7966e8ffe0195567f742f33dc25
shahidshabir055 <shahidshabir055@gmail.com>
2020-05-20 22:58:46 +0530

    Updates

---
+++ precache-manifest.4644716c2ba805cc6b497953ec01ff7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a76231a8795bd3ba926a1677af55c38",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "774b15d891339728d527",
+    "url": "/kanban/static/css/main.40a85bbe.chunk.css"
+  },
+  {
+    "revision": "1aec6c0ea328da35f9bb",
+    "url": "/kanban/static/js/2.e057af28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban/static/js/2.e057af28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774b15d891339728d527",
+    "url": "/kanban/static/js/main.d6cb860d.chunk.js"
+  },
+  {
+    "revision": "c16ebdf3a25592cf81f1",
+    "url": "/kanban/static/js/runtime-main.73f720d1.js"
+  }
+]);
